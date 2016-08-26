1df531f817beb013f0cdedee840d5004a1166db6
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2016-08-25 22:40:12 -0700

    It's more like that old iPod game than Pong
    Oh, well

--- PongWorkshop/src/pongworkshop/PongWorkshop.java
+++ PongWorkshop/src/pongworkshop/PongWorkshop.java
@@ -3,18 +3,36 @@
 package pongworkshop;
 import javax.swing.*;
 import java.awt.*;
-
-public class PongWorkshop {
-     class MyCanvas extends JComponent{
-         @Over
-        void paint(Graphics g){
-            g.drawRect(270,540, 25, 25);
-        }
+class MyCanvas extends JComponent{
+    @Override
+    public void paint(Graphics g){
+        g.drawRect(200, 480, 100, 20);
+        g.fillRect(200, 480, 100, 20);
+        g.drawRect(100, 100, 25, 20);
+        g.fillRect(100, 100, 25, 20);
+        g.setColor(Color.yellow);
+        
+        
+                
+        
         
     }
+    
+    
+}
+public class PongWorkshop {
 
     
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(540, 540);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("The Game Pong");
+        frame.getContentPane().setBackground(Color.WHITE);
+        
+        frame.getContentPane().add(new MyCanvas());
+        frame.setVisible(true);
+        
         
        
     }
