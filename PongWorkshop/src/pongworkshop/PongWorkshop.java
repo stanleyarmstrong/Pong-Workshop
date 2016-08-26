b029acead2d25f77d2b2793879f4ed5b75bd35b9
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2016-08-25 21:26:54 -0700

    This is the start of my Pong Workshop for Hack Club
    It will be coded in Java and JavaScript giving the user a choice on
    whether they would like to code in a specific language.

---
+++ PongWorkshop/src/pongworkshop/PongWorkshop.java
@@ -0,0 +1,22 @@
+
+
+package pongworkshop;
+import javax.swing.*;
+import java.awt.*;
+
+public class PongWorkshop {
+     class MyCanvas extends JComponent{
+         @Over
+        void paint(Graphics g){
+            g.drawRect(270,540, 25, 25);
+        }
+        
+    }
+
+    
+    public static void main(String[] args) {
+        
+       
+    }
+    
+}
