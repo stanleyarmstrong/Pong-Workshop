94066eab4e356bb942b1541db227b1f351d35012
stonewallstan <stanleyarmstrong31@gmail.com>
2016-08-27 23:10:12 -0700

    Added the Ball to the Pong Game
    I added an ellipse to represent the ball. This addition creates the
    playing material for playing the game. I added a color to this. It  is
    red.

--- PongWorkshop/src/pongworkshop/PongWorkshop.java
+++ PongWorkshop/src/pongworkshop/PongWorkshop.java
@@ -3,15 +3,24 @@
 package pongworkshop;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Toolkit;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 class MyCanvas extends JComponent{
     @Override
     public void paint(Graphics g){
         g.drawRect(200, 480, 100, 20);
         g.setColor(Color.blue);
         g.fillRect(200, 480, 100, 20);
-        g.drawRect(100, 100, 25, 20);
+        g.drawRect(200, 0, 100, 20);
         g.setColor(Color.BLACK);
-        g.fillRect(100, 100, 25, 20);
+        g.fillRect(200, 0, 100, 20);
+        g.drawOval(250, 200, 25, 25);
+        g.setColor(Color.red);
+        g.fillOval(250, 200, 25, 25);
+        
         
         
         
@@ -22,7 +31,8 @@ class MyCanvas extends JComponent{
     
     
 }
-public class PongWorkshop {
+
+public class PongWorkshop implements ActionListener{
 
     
     public static void main(String[] args) {
