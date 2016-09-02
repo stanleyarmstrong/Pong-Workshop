f463b63144ef16a6e755de65628e4b5d4ef8419a
Stanley Armstrong <stanleyarmstrong31@gmail.com>
2016-09-01 22:22:18 -0700

    Added some new things
    Go lang to add some Backend stuff and maybe make it front end? Probably
    not because we could do it with JavaScript. But we'll see

---
+++ stuff.go
@@ -0,0 +1,18 @@
+package  main
+import(
+  "fmt"
+  "math/rand"
+)
+func items(ball int,paddle int ){
+  ball = this.ball
+  paddle = this.paddle
+  fmt.println(ball)
+  fmt.println(paddle)
+
+
+}
+func main()  {
+  items(1,2)
+  
+
+}
