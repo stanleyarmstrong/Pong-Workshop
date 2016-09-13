014e516ebc0e20d82998357e53fbe752fbc3cd8a
stonewallstan <stanleyarmstrong31@gmail.com>
2016-09-12 22:05:05 -0700

    Added the Script Because Soemhow that got deleted

--- script.js
+++ script.js
@@ -7,59 +7,57 @@ function draw(){
   var x = 5;
   var y = 5;
 
-  background(0,125,255);
+  background(0,0,0);
   var circle = ellipse(250,250,25,25);
   smooth();
   rect(250,490,100,10);
-<<<<<<< HEAD
+//<<<<<<< HEAD
 //<<<<<<< HEAD
 //=======
 //<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+//=======
+//<<<<<<< HEAD
 
 
 
 
   if(keyIsPressed === true){
-    circle.accelerationY(10);
+    accelerationY = 10;
   }
+  console.log(keyIsPressed);
 
 
 
 
 
 
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
+//=======
+//<<<<<<< HEAD
+//=======
+//<<<<<<< HEAD
+//>>>>>>> origin/master
 
 
 //=======
 //<<<<<<< HEAD
 //>>>>>>> origin/master
-  if(keyIsPressed === true){
-    circle.accelerationY = 10;
-  }
+
 
 //<<<<<<< HEAD
 //=======
 //=======
-  rect(250,10,100,10);
-  ellipse.setColor(0,0,0);
-  ellipse.fill(250,250,50);
-<<<<<<< HEAD
+
+
+//<<<<<<< HEAD
 //>>>>>>> origin/master
 //>>>>>>> origin/master
 //>>>>>>> origin/master
-=======
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
->>>>>>> origin/master
+//=======
+//>>>>>>> origin/master
+//>>>>>>> origin/master
+//>>>>>>> origin/master
+//>>>>>>> origin/master
+//>>>>>>> origin/master
 
 
 
